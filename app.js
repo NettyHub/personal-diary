b14c07553995770071ed1b1af8ed2384cf983819
NettyHub <ramiz38izhutin@gmail.com>
2024-05-28 16:07:17 +0300

    implemented feature

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const appRoutes = require('./routes.js');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const appRoutes = require('./routes.js'); 
+
 const app = express();
+
+app.use(morgan('tiny'));
+
 app.use(cors());
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
 app.use('/api', appRoutes);
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
