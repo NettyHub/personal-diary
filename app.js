98a59ca03cd37b39eae3285dbebeda49f052854e
NettyHub <ramiz38izhutin@gmail.com>
2024-06-01 14:14:16 +0300

    cleaning

--- app.js
+++ app.js
@@ -4,24 +4,20 @@ const cors = require('cors');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const appRoutes = require('./routes.js');
-
 const app = express();
-
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({
+  extended: true
+}));
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 });
-
 app.use(limiter);
 app.use('/api', appRoutes);
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
